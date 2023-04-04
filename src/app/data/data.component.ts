f0bf8521c0f490dba61a59ba9f6e32d0e8dcea3c
Fun Of Heuristic <funoheuristic@gmail.com>
2023-04-04 22:52:22 +0200

    initial commit

---
+++ src/app/data/data.component.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.css'],
+})
+export class DataComponent implements OnInit {
+  tableData: Product[] = [];
+  loader: boolean = false;
+  error: boolean = false;
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  ngOnInit(): void {
+    const keyword = this.route.snapshot.queryParamMap.get('key');
+    this.loader = true;
+    this.http
+      .get<Product[]>(`http://localhost:3000/search/${keyword}`)
+      .pipe(
+        catchError(() => {
+          this.error = true;
+          return of([]);
+        })
+      )
+      .subscribe((data) => {
+        this.tableData = data;
+        this.loader = false;
+      });
+  }
+}
+
+interface Product {
+  asin: string;
+  title: string;
+  price: number;
+  buyBox: string;
+  sales: boolean;
+  category: string;
+  fbaFee: string;
+  sale: number;
+  brand: string;
+}
