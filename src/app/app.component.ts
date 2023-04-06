2e934a1ec8a0e907568c07cbc3003a830d15ea60
Fun Of Heuristic <funoheuristic@gmail.com>
2023-04-06 11:12:21 +0200

    updated scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'chrome-base';
+  constructor(private router: Router) {}
+  updateUrlParam(event) {
+    const value = event.target.value;
+    window.location.search = `?key=${value}`;
+  }
 }
